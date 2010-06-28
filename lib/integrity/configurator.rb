7141f6bd061d145aca579ba2ae8406be24614fc8
Simon Rozet <simon@rozet.name>
2010-06-28 22:51:50 +0200

    make it a config

--- lib/integrity/configurator.rb
+++ lib/integrity/configurator.rb
@@ -51,5 +51,14 @@ module Integrity
     def pass(v)
       Integrity.app.set(:pass, v)
     end
+
+    def webkit_notifier=(v)
+      Integrity.app.set(:webkit_notifier, true)
+    end
+
+    def webkit_notifier?
+      Integrity.app.respond_to?(:webkit_notifier) &&
+        Integrity.app.webkit_notifier?
+    end
   end
 end
